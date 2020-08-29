2a87299f9a5df6210104eff6e19fced0c050a7af
Angel Ponce <60164099+Angel-Ponce@users.noreply.github.com>
2020-08-28 19:44:02 -0600

    -Small details at info-

--- js/info.js
+++ js/info.js
@@ -18,9 +18,19 @@ class info {
         text.classList.add("text");
         text.innerHTML =
             "Hola mi nombre es Angel Ponce, estudiante de ingeniería en sistemas en la Universidad San Carlos de Guatemala, tengo 19 años de edad. Me gusta desarrollar webapps y también programas desktop, algunas de mis especialidades son: Java, HTML, SCSS, JavaScript, y bases de datos relacionales en MySQL y postgreSQL. Esta es la documentación de uno de los mejores proyectos que he realizado en mi experiencia como desarrollador, se trata de un Ajedrez, el cuál realicé en web y tiene 2 diferentes tipos de modalidades de juego, para un solo jugador (juega contra la pc) y para dos jugadores (en la misma pc) en esta documentación encontrarás información acerca de los metodos y clases que utilice en el proyecto. El diseño de las piezas se lo agradezco a Dojo Estudios, una gran empresa de desarrollo gráfico, dirigida por un amigo mio.";
+        let divGH  = document.createElement("div");
+        divGH.classList.add("gh");
+        let imgCat = document.createElement("img");
+        imgCat.src = "./images/cat.png";
+        let linkA = document.createElement("a");
+        linkA.href = "https://github.com/Angel-Ponce/chess-11";
+        linkA.target = "_blank";
+        linkA.appendChild(imgCat);
+        divGH.appendChild(linkA);
         this.containerInfo.appendChild(img);
         this.containerInfo.appendChild(text);
         this.container.appendChild(this.containerInfo);
+        this.container.appendChild(divGH);
     });
   }
 
