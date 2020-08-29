6c6c682735b10275017ec5a9bc5ee93d3083006c
Angel Ponce <60164099+Angel-Ponce@users.noreply.github.com>
2020-08-28 23:25:17 -0600

    -start with board.js-

--- js/board.js
+++ js/board.js
@@ -20,8 +20,47 @@ class board {
           text1.classList.add("text");
           text1.innerHTML = "La clase board literalmente hace referencia al tablero de juego, en donde se desarrolla toda la lógica y manipulación del mismo.";
 
+          let codeExample1 = document.createElement("div");
+          codeExample1.classList.add("textCode");
+          let arr1 = ["export class board {",
+                    "--constructor(size) {",
+                    "----this.size = size;",
+                    `----this.main = document.querySelector(".main");`,
+                    `----this.deathBlack = document.querySelector("#blacks");`,
+                    `----this.deathWhite = document.querySelector("#whites");`,
+                    `----this.singleplayer = document.querySelector(".singleplayer");`,
+                    `----this.multiplayer = document.querySelector(".multiplayer");`,
+                    `----this.turnDer = document.querySelector("#der");`,
+                    `----this.turnIzq = document.querySelector("#izq");`,
+                    `----this.tiles = [];`,
+                    "----this.tilesDB = [];",
+                    "----this.tilesDW = [];",
+                    "----this.wayDown = [0, 1, 2, 3, 4, 5, 6, 7];",
+                    "----this.wayUp = [56, 57, 58, 59, 60, 61, 62, 63];",
+                    "----this.wayLeft = [0, 8, 16, 24, 32, 40, 48, 56];",
+                    "----this.wayRight = [7, 15, 23, 31, 39, 47, 55, 63];",
+                    "----this.click = 0;",
+                    "----this.pInicial;",
+                    "----this.pFinal;",
+                    "----this.turn = 'white';",
+                    "----this.modal = 'multi';","}"];
+          this.putLinesOfCode(arr1,codeExample1);
+
+          let text2 = document.createElement("div");
+          text2.classList.add("text");
+          text2.innerHTML = "Esta clase tiene más propiedades del DOM que las demás, debido a que por ser el tablero principal del juego necesita acceso a los elementos principales del index.html, de manera que la lógica pueda reservarse en esta clase. \
+          También guarda propiedades mismas del juego, tal como los límites de restricción para algunas piezas, la modalidad del juego, el control de clicks, el control de intercambio de pieza al movimiento, etc.";
           
-          let elements = [title,text1];
+          let text3 = document.createElement("div");
+          text3.classList.add("text");
+          text3.innerHTML = "El método 'createSpaceOfDeaths' recibe como parámetros la propiedad de los tiles, puede des tileDB o tileDW, que serán los tiles lógicos para el espacio de las piezas que sean eliminadas en un juego \
+          el siguiente parámetro es el elemento del DOM donde se deberán dibujar, puede ser deathBlack o deathWhite";
+
+          let codeExample2 = document.createElement("div");
+          codeExample2.classList.add("textCode");
+          codeExample2.innerHTML = "createSpaceOfDeahts(tilesName,deathName)";
+
+          let elements = [title,text1,codeExample1,text2,text3,codeExample2];
           this.putElementsOnContainers(elements,this.containerBoard);
           this.container.appendChild(this.containerBoard);
       });
